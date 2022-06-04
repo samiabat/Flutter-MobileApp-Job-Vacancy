d4cabc8a3b8dc2160c2af61b5c32e1924336be6d
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-06-04 12:59:43 +0300

    bloc test

--- frontend/job_vacancy/test/job_test.dart
+++ frontend/job_vacancy/test/job_test.dart
@@ -1,45 +1,17 @@
 
-// void main(){
-//   final JobRepository jobRepository;
-//   group("Job unit test",(){
-//    blocTest(
-//      "Emits [] when nothing is added",
-//     build:()=>JobBloc(jobRepository),
-//     expect:()=>[],
-//    );
-//    blocTest(
-//      "Emit [JobLoading]",
-//      build:()=>JobBloc(jobRepository),
-//      act:(bloc)=>bloc.add(JobLoad(jobRepository)),
-//      expect:()=>JobLoading(),
-//    );
+void main(){
+  final JobRepository jobRepository;
+  group("Job unit test",(){
+  jobRepository=JobRepository();
 
-//   });
-// }
-// Description text to show when test runs.
-// build => is used to create BLoC or Cubit object for test.
-// act => is used to add event or to call method for trigger BLoC.
-// expect => is a simple Iterable, which is used to ensure that act called from previous parameter will return State or States noted in here.
-
-
-//  class MockJobRepository extends Mock implements JobRepository{}
-//  void main(){
-//    MockJobRepository mockJob;
-//    setUp((){
-//      mockJob=MockJobRepository();
-
-//    });
-//    group('get jobs ',(){});
-//  }
-
-
-
-
-
-
-
-
-
+   blocTest<JobBloc,JobState>(
+     "Emit [JobLoading]",
+     build:()=>JobBloc(jobRepository:jobRepository),
+     act:(bloc)=>bloc.add(JobLoad(jobRepository)),
+     expect:()=><JobEvent>[JobLoading()],
+   );
+  });
+}
 
 
 
